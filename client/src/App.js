ec0c93693395ffe6864a639f2a22f548d14db520
Matt Dudzik <mdudzik92@gmail.com>
2020-11-29 19:01:31 -0600

    Set up client folder to run server and front end concurrently, implemented reactstrap

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import AppNavbar from './components/AppNavbar';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AppNavbar />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+// What was there upon install instead of class App extends^
+// function App() {
+//   return (
+//     <div className="App">
+//       <AppNavbar />
+//     </div>
+//   );
+// }
+
+// export default App;
